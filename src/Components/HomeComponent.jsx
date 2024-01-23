9cee6188e44d764e4d677888b5cc9128902de470
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-23 15:29:50 +0200

    Update HomeComponent.jsx

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useFetch from './useFetch'
+import { MonsterCardComponent } from './MonsterCardComponent';
+import './MonsterCardCSS.css'
+
 
 export const HomeComponent = () => {
-  return (
-    <div>
-        <h1>Zaglavieto</h1>
-        <h2>odbcojdsbjdscnksmc</h2>
-    </div>
- )
+
+    const { data } = useFetch('https://jsonplaceholder.typicode.com/users');
+
+    const [filtered, setFiltered] = useState(data)
+
+    return (
+        <div className="home-component">
+            <h1>Monsters Rolodex</h1>
+            <input type="search"
+                className='search-box'
+                placeholder='search monster'
+                onChange={(e) => {
+                    const searchMonster = e.target.value.toLocaleLowerCase()
+                    console.log(searchMonster);
+
+                    const filteredData = data.filter((monster) => {
+                        return monster.name.toLocaleLowerCase().includes(searchMonster);
+                        
+                    })
+
+                    setFiltered(filteredData);
+                  
+                    
+                }}
+            />
+            {filtered && <MonsterCardComponent data={filtered} />}
+        </div>
+
+    )
 }
