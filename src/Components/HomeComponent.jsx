5b0e1634557c3edafb281bbff2c0e902a68b379f
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-24 09:38:56 +0200

    Update HomeComponent.jsx

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import useFetch from './useFetch'
 import { MonsterCardComponent } from './MonsterCardComponent';
 import './MonsterCardCSS.css'
@@ -7,8 +7,29 @@ import './MonsterCardCSS.css'
 export const HomeComponent = () => {
 
     const { data } = useFetch('https://jsonplaceholder.typicode.com/users');
+    const [filtered, setFiltered] = useState(data);
+    const [searchMonster, setSearchMonster] = useState('');
 
-    const [filtered, setFiltered] = useState(data)
+    useEffect(() => {
+        const fetchData = async () => {
+ 
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            const data = await response.json();
+          
+            const filteredData = data.filter((monster) =>
+                monster.name.toLowerCase().includes(searchMonster.toLowerCase())
+            );
+
+            setFiltered(filteredData);
+        };
+
+        fetchData();
+        
+    }, [searchMonster]); // Използване на searchMonster като зависимост, за да следим промените в него
+
+    const handleChange = (e) => {
+        setSearchMonster(e.target.value);
+    };
 
     return (
         <div className="home-component">
@@ -16,19 +37,7 @@ export const HomeComponent = () => {
             <input type="search"
                 className='search-box'
                 placeholder='search monster'
-                onChange={(e) => {
-                    const searchMonster = e.target.value.toLocaleLowerCase()
-                    console.log(searchMonster);
-
-                    const filteredData = data.filter((monster) => {
-                        return monster.name.toLocaleLowerCase().includes(searchMonster);
-                        
-                    })
-
-                    setFiltered(filteredData);
-                  
-                    
-                }}
+                onChange={handleChange}
             />
             {filtered && <MonsterCardComponent data={filtered} />}
         </div>
