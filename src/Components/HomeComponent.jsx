91cc8b32e586a0449e75902031cf090a32b6d799
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-25 11:08:52 +0200

    Update HomeComponent.jsx

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -1,35 +1,34 @@
 import React, { useState, useEffect } from 'react'
-import useFetch from './useFetch'
 import { MonsterCardComponent } from './MonsterCardComponent';
 import './MonsterCardCSS.css'
 
 
 export const HomeComponent = () => {
 
-    const { data } = useFetch('https://jsonplaceholder.typicode.com/users');
-    const [filtered, setFiltered] = useState(data);
+    const [filtered, setFiltered] = useState([]);
     const [searchMonster, setSearchMonster] = useState('');
 
     useEffect(() => {
-        const fetchData = async () => {
- 
-            const response = await fetch('https://jsonplaceholder.typicode.com/users');
-            const data = await response.json();
-          
-            const filteredData = data.filter((monster) =>
-                monster.name.toLowerCase().includes(searchMonster.toLowerCase())
-            );
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => setFiltered(users))
+    }, []);
 
-            setFiltered(filteredData);
-        };
 
-        fetchData();
-        
-    }, [searchMonster]); // Използване на searchMonster като зависимост, за да следим промените в него
 
     const handleChange = (e) => {
-        setSearchMonster(e.target.value);
+        const searchString = e.target.value.toLowerCase()
+        setSearchMonster(searchString);
     };
+
+
+    const filteredData = filtered.filter((monster) => {
+        return monster.name.toLowerCase().includes(searchMonster.toLowerCase())
+
+    });
+
+   
+
 
     return (
         <div className="home-component">
@@ -39,7 +38,7 @@ export const HomeComponent = () => {
                 placeholder='search monster'
                 onChange={handleChange}
             />
-            {filtered && <MonsterCardComponent data={filtered} />}
+            <MonsterCardComponent data={filteredData} />
         </div>
 
     )
