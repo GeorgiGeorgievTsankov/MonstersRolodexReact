6364c3e6a0c0d8640803a9f1d2dfe0a201efc0db
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-04-22 20:54:08 +0300

    Update MonsterCardComponent.jsx

--- src/Components/MonsterCardComponent.jsx
+++ src/Components/MonsterCardComponent.jsx
@@ -8,6 +8,7 @@ export const MonsterCardComponent = ({ data }) => {
         <div className="card-component">
             {data.map((monster) => (
                 <div className="card" key={monster.id}>
+                    <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt="monster-image" />
                     <h1>{monster.name}</h1>
                     <p>{monster.email}</p>
                 </div>
